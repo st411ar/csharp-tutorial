0c33106c9e4b7e50d88f2014585f991dd7641c85
Vitaly Kiselev <st411ar@ya.ru>
2019-05-27 17:33:32 +0300

    implemented example of a culture-specific interpolated string

--- string-interpolation-advanced/Program.cs
+++ string-interpolation-advanced/Program.cs
@@ -44,6 +44,24 @@ namespace StringInterpolationAdvanced
             for (int i = 0; i < 7; i++) {
                 Console.WriteLine($"Coin flip: {(rand.NextDouble() < 0.5 ? "heads" : "tails")}");
             }
+
+
+            var cultures = new System.Globalization.CultureInfo[]
+            {
+                System.Globalization.CultureInfo.GetCultureInfo("en-US"),
+                System.Globalization.CultureInfo.GetCultureInfo("en-GB"),
+                System.Globalization.CultureInfo.GetCultureInfo("nl-NL"),
+                System.Globalization.CultureInfo.GetCultureInfo("ru-RU"),
+                System.Globalization.CultureInfo.InvariantCulture
+            };
+
+            date = DateTime.Now;
+            var number = 31_415_926.536;
+            FormattableString message = $"{date,20}{number,20:n3}";
+            foreach (var culture in cultures) {
+                var cultureSpecificMessage = message.ToString(culture);
+                Console.WriteLine($"{culture.Name,-10}{cultureSpecificMessage}");
+            }
         }
     }
 }
