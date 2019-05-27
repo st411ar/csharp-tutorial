aaa77e644454f27975d5f14bd30e763bd947f5f9
Vitaly Kiselev <st411ar@ya.ru>
2019-05-27 16:03:39 +0300

    added example of interpolation expressions field width and alignment for the local environment tutorial

--- string-interpolation-tutorial/Program.cs
+++ string-interpolation-tutorial/Program.cs
@@ -37,6 +37,19 @@ namespace StringInterpolationTutorial
             var price = 1.99m;
             var unit = Unit.item;
             Console.WriteLine($"On {dateTime:d}, the price of {element} was {price:c2} per {unit}.");
+
+            var titles = new Dictionary<string, string>()
+            {
+                ["Doyle, Arthur Conan"] = "Hound of the Baskervilles, The",
+                ["London, Jack"] = "Call of the Wild, The",
+                ["Shakespeare, William"] = "Tempest, The"
+            };
+            Console.WriteLine("Author and Title List");
+            Console.WriteLine();
+            Console.WriteLine($"|{"Author",-25}|{"Title",30}|");
+            foreach (var title in titles) {
+                Console.WriteLine($"|{title.Key,-25}|{title.Value,30}|");
+            }
         }
     }
 }
