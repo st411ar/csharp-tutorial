4595c7094f47ec4e688e4d44f5d6142401db2fe6
Vitaly Kiselev <st411ar@ya.ru>
2019-05-27 17:53:35 +0300

    refactored string interpolation tutorial logic

--- string-interpolation-tutorial/Program.cs
+++ string-interpolation-tutorial/Program.cs
@@ -7,15 +7,35 @@ namespace StringInterpolationTutorial
     {
         public enum Unit { item, kilogram, gram, dozen };
 
+
         static void Main(string[] args)
+        {
+            Intro();
+
+            FormatDifferentDataTypes();
+            FieldWidthAndAlignment();
+            Combine();
+
+            FormatDifferentDataTypesForLocalEnv();
+            FieldWidthAndAlignmentForLocalEnv();
+        }
+
+
+        static void Intro()
         {
             var name = "Vitaly";
             Console.WriteLine($"Hello, {name}. It's a pleasure to meet you!");
+        }
 
+        static void FormatDifferentDataTypes()
+        {
             var item = (Name: "eggplant", Price: 1.99m, perPackage: 3);
             var date = DateTime.Now;
             Console.WriteLine($"On {date:d}, the price of {item.Name} was {item.Price:c2} per {item.perPackage} items.");
+        }
 
+        static void FieldWidthAndAlignment()
+        {
             var inventory = new Dictionary<string, int>()
             {
                 ["hammer, ball pein"] = 18,
@@ -25,19 +45,28 @@ namespace StringInterpolationTutorial
             Console.WriteLine($"Inventory on {DateTime.Now:d}");
             Console.WriteLine(" ");
             Console.WriteLine($"|{"Item",-25}|{"Quantity",10}|");
-            foreach (var entry in inventory)
+            foreach (var item in inventory)
             {
-                Console.WriteLine($"|{entry.Key,-25}|{entry.Value,10}|");
+                Console.WriteLine($"|{item.Key,-25}|{item.Value,10}|");
             }
+        }
 
+        static void Combine()
+        {
             Console.WriteLine($"[{DateTime.Now,-20:d}] Hour [{DateTime.Now,-10:HH}] [{1063.342,15:N2}] feet");
+        }
 
-            var element = new Vegetable("eggplant");
-            var dateTime = DateTime.Now;
+        static void FormatDifferentDataTypesForLocalEnv()
+        {
+            var item = new Vegetable("eggplant");
+            var date = DateTime.Now;
             var price = 1.99m;
             var unit = Unit.item;
-            Console.WriteLine($"On {dateTime:d}, the price of {element} was {price:c2} per {unit}.");
+            Console.WriteLine($"On {date:d}, the price of {item} was {price:c2} per {unit}.");
+        }
 
+        static void FieldWidthAndAlignmentForLocalEnv()
+        {
             var titles = new Dictionary<string, string>()
             {
                 ["Doyle, Arthur Conan"] = "Hound of the Baskervilles, The",
