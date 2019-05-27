1f07e924c0c0ebd11e26c50a5161e8a5aaa33339
Vitaly Kiselev <st411ar@ya.ru>
2019-05-27 16:27:33 +0300

    created string interpolation advanced tutorial

---
+++ string-interpolation-advanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringInterpolationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
