7450bf7bbd9a5fc5dae6a226f97c09b6252f9141
Vitaly Kiselev <st411ar@ya.ru>
2019-05-27 14:40:06 +0300

    added example of interpolation expressions field width and alignment

--- string-interpolation-tutorial/Program.cs
+++ string-interpolation-tutorial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringInterpolationTutorial
 {
@@ -12,6 +13,20 @@ namespace StringInterpolationTutorial
             var item = (Name: "eggplant", Price: 1.99m, perPackage: 3);
             var date = DateTime.Now;
             Console.WriteLine($"On {date:d}, the price of {item.Name} was {item.Price:c2} per {item.perPackage} items.");
+
+            var inventory = new Dictionary<string, int>()
+            {
+                ["hammer, ball pein"] = 18,
+                ["hammer, cross pein"] = 5,
+                ["screwdriver, Phillips #2"] = 14
+            };
+            Console.WriteLine($"Inventory on {DateTime.Now:d}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"|{"Item",-25}|{"Quantity",10}|");
+            foreach (var unit in inventory)
+            {
+                Console.WriteLine($"|{unit.Key,-25}|{unit.Value,10}|");
+            }
         }
     }
 }
