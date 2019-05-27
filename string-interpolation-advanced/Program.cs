26492f84ede3719e697f2eaa798c7413df661090
Vitaly Kiselev <st411ar@ya.ru>
2019-05-27 16:36:20 +0300

    implemented introduction example

--- string-interpolation-advanced/Program.cs
+++ string-interpolation-advanced/Program.cs
@@ -6,7 +6,12 @@ namespace StringInterpolationAdvanced
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 3;
+            double b = 4;
+            Console.WriteLine($"Area of the right triangle with legs of {a} and {b} is {0.5 * a * b}");
+            Console.WriteLine($"Length of the hypotenuse of the right triangle with legs of {a} and {b} is {CalculateTypotenuse(a, b)}");
+
+            double CalculateTypotenuse(double leg1, double leg2) => Math.Sqrt(leg1 * leg1 + leg2 * leg2);
         }
     }
 }
