b2e53e662eb7139ba7a38fe3a7487e66dc5ef036
Vitaly Kiselev <st411ar@ya.ru>
2019-05-28 21:19:41 +0300

    completed nullable reference types introduction tutorial

--- nullable-introduction/Program.cs
+++ nullable-introduction/Program.cs
@@ -6,7 +6,27 @@ namespace NullableIntroduction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var surveyRun = new SurveyRun();
+            surveyRun.AddQuestion(QuestionType.YesNo, "Has your code ever thrown a NullReferenceException?");
+            surveyRun.AddQuestion(new SurveyQuestion(QuestionType.Number, "How many times (to the nearest 100) has that happened?"));
+            surveyRun.AddQuestion(QuestionType.Text, "What is your favorive color?");
+
+            surveyRun.PerformSurvey(50);
+
+            foreach (var participant in surveyRun.AllParticipants)
+            {
+                Console.WriteLine($"Participant: {participant.Id}");
+                if (participant.AnsweredSurvey)
+                {
+                    for (int i = 0; i < surveyRun.Questions.Count; i++)
+                    {
+                        var answer = participant.Answer(i);
+                        Console.WriteLine($"\t{surveyRun.GetQuestion(i).QuestionText} : {answer}");
+                    }
+                } else {
+                    Console.WriteLine("\tNo responses");
+                }
+            }
         }
     }
 }
