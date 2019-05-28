3af4e53ff7ec65591f119e21e1837d8b6b21fee1
Vitaly Kiselev <st411ar@ya.ru>
2019-05-28 18:06:27 +0300

    added local functions example

--- c-sharp-7-tutorial/Program.cs
+++ c-sharp-7-tutorial/Program.cs
@@ -11,7 +11,8 @@ namespace CSharp7Tutorial
             // Tuples();
             // TypePatternWithIsExpression();
             // TypePatternWithSwitchStatement();
-            RefLocalAndReturn();
+            // RefLocalAndReturn();
+            LocalFunctions();
         }
 
         static void OutVariableDeclaration()
@@ -131,6 +132,63 @@ namespace CSharp7Tutorial
             Console.WriteLine(valItem);
             valItem = 24;
             Console.WriteLine(sourceMatrix[4, 2]);
+        }
+
+        static void LocalFunctions()
+        {
+            int LocalFunctionFactorial(int n)
+            {
+                return nthFactorial(n);
+
+                int nthFactorial(int number) => (number < 2) ? 1 : number * nthFactorial(number - 1);
+            }
+
+            Console.WriteLine(LocalFunctionFactorial(6));
+
+
+            IEnumerable<char> AlphabetSubset(char start, char end)
+            {
+                if (start < 'a' || start > 'z')
+                {
+                    throw new ArgumentOutOfRangeException(paramName: nameof(start), message: "start must be a letter");
+                }
+                if (end < 'a' || end > 'z')
+                {
+                    throw new ArgumentOutOfRangeException(paramName: nameof(start), message: "end must be a letter");
+                }
+                if (end <= start)
+                {
+                    throw new ArgumentException($"{nameof(end)} must be greater than {nameof(start)}");
+                }
+
+                for (var c = start; c < end; c++)
+                {
+                    yield return c;
+                }
+            }
+
+            try
+            {
+                var resultSet1 = AlphabetSubset('d', 'r');
+                var resultSet2 = AlphabetSubset('f', 'a');
+                Console.WriteLine("iterators created");
+
+                foreach (var thing1 in resultSet1)
+                {
+                    Console.Write($"{thing1}, ");
+                }
+                Console.WriteLine();
+
+                foreach (var thing2 in resultSet2)
+                {
+                    Console.Write($"{thing2}, ");
+                }
+                Console.WriteLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Caught an argument exception");
+            }
         }
 
 
