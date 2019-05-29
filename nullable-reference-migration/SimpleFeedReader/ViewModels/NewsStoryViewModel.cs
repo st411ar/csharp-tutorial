b554355098f3174eedb06b81b2d639c742c2153e
Vitaly Kiselev <st411ar@ya.ru>
2019-05-29 18:37:15 +0300

    limited the scope of warnings

--- nullable-reference-migration/SimpleFeedReader/ViewModels/NewsStoryViewModel.cs
+++ nullable-reference-migration/SimpleFeedReader/ViewModels/NewsStoryViewModel.cs
@@ -3,11 +3,13 @@
 namespace SimpleFeedReader.ViewModels
 {
     // <SnippetStarterViewModel>
+    #nullable enable
     public class NewsStoryViewModel
     {
         public DateTimeOffset Published { get; set; }
         public string Title { get; set; }
         public string Uri { get; set; }
     }
+    #nullable restore
     // </SnippetStarterViewModel>
 }
