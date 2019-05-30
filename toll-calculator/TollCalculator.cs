e44c06273c5d19b1fa6a106af49b8accf7afe4c3
Vitaly Kiselev <st411ar@ya.ru>
2019-05-30 18:52:10 +0300

    added peak pricing

--- toll-calculator/TollCalculator.cs
+++ toll-calculator/TollCalculator.cs
@@ -38,5 +38,68 @@ namespace TollCalculator
                 {}              => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
                 null            => throw new ArgumentNullException(nameof(vehicle))
             };
+
+        public decimal PeakTimePremium(DateTime timeOfToll, bool inbound) =>
+            (IsWeekDay(timeOfToll), GetTimeBand(timeOfToll), inbound) switch
+            {
+                (true, TimeBand.MorningRush, true) => 2.00m,
+                (true, TimeBand.Daytime, _) => 1.50m,
+                (true, TimeBand.EveningRush, false) => 2.00m,
+                (true, TimeBand.Overnight, _) => 0.75m,
+                (_, _, _) => 1.00m
+            };
+
+        public decimal PeakTimePremiumFull(DateTime timeOfToll, bool inbound) =>
+            (IsWeekDay(timeOfToll), GetTimeBand(timeOfToll), inbound) switch
+            {
+                (true,  TimeBand.MorningRush, true)  => 2.00m,
+                (true,  TimeBand.MorningRush, false) => 1.00m,
+                (true,  TimeBand.Daytime,     true)  => 1.50m,
+                (true,  TimeBand.Daytime,     false) => 1.50m,
+                (true,  TimeBand.EveningRush, true)  => 1.00m,
+                (true,  TimeBand.EveningRush, false) => 2.00m,
+                (true,  TimeBand.Overnight,   true)  => 0.75m,
+                (true,  TimeBand.Overnight,   false) => 0.75m,
+                (false, TimeBand.MorningRush, true)  => 1.00m,
+                (false, TimeBand.MorningRush, false) => 1.00m,
+                (false, TimeBand.Daytime,     true)  => 1.00m,
+                (false, TimeBand.Daytime,     false) => 1.00m,
+                (false, TimeBand.EveningRush, true)  => 1.00m,
+                (false, TimeBand.EveningRush, false) => 1.00m,
+                (false, TimeBand.Overnight,   true)  => 1.00m,
+                (false, TimeBand.Overnight,   false) => 1.00m,
+            };
+
+
+        private static bool IsWeekDay(DateTime timeOfToll) =>
+            timeOfToll.DayOfWeek switch
+            {
+                DayOfWeek.Saturday  => false,
+                DayOfWeek.Sunday    => false,
+                _                   => true
+            };
+
+        private enum TimeBand
+        {
+            MorningRush,
+            Daytime,
+            EveningRush,
+            Overnight
+        }
+
+        private static TimeBand GetTimeBand(DateTime timeOfToll)
+        {
+            int hour = timeOfToll.Hour;
+            if (hour < 6)
+                return TimeBand.Overnight;
+            else if (hour < 10)
+                return TimeBand.MorningRush;
+            else if (hour < 16)
+                return TimeBand.Daytime;
+            else if (hour < 20)
+                return TimeBand.EveningRush;
+            else
+                return TimeBand.Overnight;
+        }
     }
 }
