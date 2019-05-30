f6ae54130193fe86847e72e0d2592b217aaa6778
Vitaly Kiselev <st411ar@ya.ru>
2019-05-30 18:14:20 +0300

    added occupancy pricing

--- toll-calculator/TollCalculator.cs
+++ toll-calculator/TollCalculator.cs
@@ -11,10 +11,30 @@ namespace TollCalculator
         public decimal CalculateToll(object vehicle) =>
             vehicle switch
             {
-                Car c           => 2.00m,
-                Taxi t          => 3.50m,
+                Car c => c.Passengers switch
+                {
+                    0 => 2.00m + 0.5m,
+                    1 => 2.0m,
+                    2 => 2.0m - 0.5m,
+                    _ => 2.00m - 1.0m
+                },
+
+                Taxi t => t.Fares switch
+                {
+                    0 => 3.50m + 1.00m,
+                    1 => 3.50m,
+                    2 => 3.50m - 0.50m,
+                    _ => 3.50m - 1.00m
+                },
+
+                Bus b when ((double)b.Riders / (double)b.Capacity) < 0.50 => 5.00m + 2.00m,
+                Bus b when ((double)b.Riders / (double)b.Capacity) > 0.90 => 5.00m - 1.00m,
                 Bus b           => 5.00m,
+
+                DeliveryTruck t when (t.GrossWeightClass > 5000) => 10.00m + 5.00m,
+                DeliveryTruck t when (t.GrossWeightClass < 3000) => 10.00m - 2.00m,
                 DeliveryTruck t => 10.00m,
+
                 {}              => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
                 null            => throw new ArgumentNullException(nameof(vehicle))
             };
