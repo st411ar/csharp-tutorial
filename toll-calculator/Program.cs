369ee89f323d897685029d3175747c2bdf19db87
Vitaly Kiselev <st411ar@ya.ru>
2019-05-30 18:21:38 +0300

    implemented occupancy pricing

--- toll-calculator/Program.cs
+++ toll-calculator/Program.cs
@@ -11,6 +11,7 @@ namespace TollCalculator
         static void Main(string[] args)
         {
             BasicTollCalculations();
+            OccupancyPricing();
         }
 
         static void BasicTollCalculations()
@@ -36,6 +37,62 @@ namespace TollCalculator
                 Console.WriteLine("Caught an argument exception when using the wrong type");
             }
 
+            try
+            {
+                tollCalc.CalculateToll(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Caught an argument exception when using null");
+            }
+        }
+
+        static void OccupancyPricing()
+        {
+            var tollCalc = new TollCalculator();
+
+            var soloDriver = new Car();
+            var twoRideShare = new Car { Passengers = 1 };
+            var threeRideShare = new Car { Passengers = 2 };
+            var fullVan = new Car { Passengers = 5 };
+            var emptyTaxi = new Taxi();
+            var singleFare = new Taxi { Fares = 1 };
+            var doubleFare = new Taxi { Fares = 2 };
+            var fullVanPool = new Taxi { Fares = 5 };
+            var lowOccupantBus = new Bus { Capacity = 90, Riders = 15 };
+            var normalBus = new Bus { Capacity = 90, Riders = 75 };
+            var fullBus = new Bus { Capacity = 90, Riders = 85 };
+
+            var heavyTruck = new DeliveryTruck { GrossWeightClass = 7500 };
+            var truck = new DeliveryTruck { GrossWeightClass = 4000 };
+            var lightTruck = new DeliveryTruck { GrossWeightClass = 2500 };
+
+            Console.WriteLine($"The toll for a solo driver is {tollCalc.CalculateToll(soloDriver)}");
+            Console.WriteLine($"The toll for a two ride share is {tollCalc.CalculateToll(twoRideShare)}");
+            Console.WriteLine($"The toll for a three ride share is {tollCalc.CalculateToll(threeRideShare)}");
+            Console.WriteLine($"The toll for a fullVan is {tollCalc.CalculateToll(fullVan)}");
+
+            Console.WriteLine($"The toll for an empty taxi is {tollCalc.CalculateToll(emptyTaxi)}");
+            Console.WriteLine($"The toll for a single fare taxi is {tollCalc.CalculateToll(singleFare)}");
+            Console.WriteLine($"The toll for a double fare taxi is {tollCalc.CalculateToll(doubleFare)}");
+            Console.WriteLine($"The toll for a full van taxi is {tollCalc.CalculateToll(fullVanPool)}");
+
+            Console.WriteLine($"The toll for a low-occupant bus is {tollCalc.CalculateToll(lowOccupantBus)}");
+            Console.WriteLine($"The toll for a regular bus is {tollCalc.CalculateToll(normalBus)}");
+            Console.WriteLine($"The toll for a bus is {tollCalc.CalculateToll(fullBus)}");
+
+            Console.WriteLine($"The toll for a truck is {tollCalc.CalculateToll(heavyTruck)}");
+            Console.WriteLine($"The toll for a truck is {tollCalc.CalculateToll(truck)}");
+            Console.WriteLine($"The toll for a truck is {tollCalc.CalculateToll(lightTruck)}");
+
+            try
+            {
+                tollCalc.CalculateToll("this will fail");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Caught an argument exception when using the wrong type");
+            }
             try
             {
                 tollCalc.CalculateToll(null);
