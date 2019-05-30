c62833ae758ec18c15acc51610e37a1fac96e080
Vitaly Kiselev <st411ar@ya.ru>
2019-05-30 16:45:01 +0300

    created extend data capabilities using pattern matching tutorial

---
+++ toll-calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TollCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
