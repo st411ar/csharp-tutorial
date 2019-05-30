6490249f03186e23188dac8a948c7ae098049802
Vitaly Kiselev <st411ar@ya.ru>
2019-05-30 17:32:28 +0300

    implemented the basic toll calculations

--- toll-calculator/Program.cs
+++ toll-calculator/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
+using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+
 namespace TollCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tollCalc = new TollCalculator();
+
+            var car = new Car();
+            var taxi = new Taxi();
+            var bus = new Bus();
+            var truck = new DeliveryTruck();
+
+            Console.WriteLine($"The toll for a car is {tollCalc.CalculateToll(car)}");
+            Console.WriteLine($"The toll for a taxi is {tollCalc.CalculateToll(taxi)}");
+            Console.WriteLine($"The toll for a bus is {tollCalc.CalculateToll(bus)}");
+            Console.WriteLine($"The toll for a truck is {tollCalc.CalculateToll(truck)}");
+
+            try
+            {
+                tollCalc.CalculateToll("this will fail");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Caught an argument exception when using the wrong type");
+            }
+
+            try
+            {
+                tollCalc.CalculateToll(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Caught an argument exception when using null");
+            }
         }
     }
 }
