ab1401441ac41fc2fe9292269b54d194d8fc26ed
Vitaly Kiselev <st411ar@ya.ru>
2019-05-31 11:54:15 +0300

    created indexes and ranges tutorial

---
+++ ranges-indexes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RangesIndexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
