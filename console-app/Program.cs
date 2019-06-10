e8c163d9a09aaa13d58570b8c892aaa23d684c00
Vitaly Kiselev <st411ar@ya.ru>
2019-06-10 17:53:29 +0300

    implemented file reading

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace ConsoleApp
+namespace TelepromterConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines) {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
         }
     }
 }
